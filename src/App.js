4555b89923dff6610f0ccf6986a71b1e9373ad84
martin-centurion <martinalejandrocenturion@gmail.com>
2023-05-29 23:45:16 -0300

    frase

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h3>Ojalá hoy te vayas a dormir sabiendo que mereces el mundo entero.</h3>
+        <h3>"Amate mucho, sé paciente y agradece. Confia en que todo estara de tu lado. Confia en que vas por el camino correcto. Tu mejor version aparece cuando comiences a creer en ti y en tu poder de hacer que las cosas pasen".</h3>
+        <p>@culturapositiva</p>
         <h1>Que Descanses Hermosa ❤️!</h1>
+        
     </div>
   );
 }
