de4b0f6138c2ac8ce219986d609dfb51b0462514
martin-centurion <martinalejandrocenturion@gmail.com>
2023-06-01 16:35:33 -0300

    link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
            target="_blank"
            rel="noopener noreferrer"
          >
-           LINK WEBSITE
+           DEVELOPER WEBSITE LINK
          </a>
          
                         
