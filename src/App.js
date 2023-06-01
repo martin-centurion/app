6b9fbcf29540eb893d16f55b39ae82464e76a0d4
martin-centurion <martinalejandrocenturion@gmail.com>
2023-06-01 01:14:04 -0300

    api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h3>"Sin palabras de lo que te dije, pero bueno tuve un mal día, estresante, con la situacion que vivi en el trabajo y bueno con lo que te dije por whastapp lo redondee, un día de mierda sin dudas, me vino a la mente que por ahi no te guste en el video y dije mejor dejo de molestarla, espero que no te quede sensacion de que soy un tarado, no tengo perdon, lo se. Pero bueno ya que esta app es unica que siga cumpliendo la funcion."</h3>
+        <h3>"Sin palabras de lo que te dije, pero bueno tuve un mal día, estresante, con la situacion que vivi en el trabajo y bueno con lo que te dije por whastapp lo redondee, un día de mierda sin dudas, me vino a la mente que por ahi no te guste en el video y dije mejor dejo de molestarla, espero que no te quede sensacion de que soy un tarado, no tengo perdon, lo se."</h3>
         <p>De parte de Martin Centurion</p>
         <a
            className="App-link"
